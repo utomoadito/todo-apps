79667435db34b24b4bb1062dede95bcc68a5fa2d
utomoaditom <utomoadhitya@gmail.com>
2021-04-27 22:39:19 +0800

    initial upload

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="home min-h-full bg-gray-100">
+    <div class="flex flex-col">
+      <div class="flex fixed lg:w-1/3 w-full bg-blue-200 p-6 justify-between z-10">
+        Todo Apps
+      </div>
+      <div class="flex flex-col pt-24 overflow-auto">
+        <div class="flex flex-col">
+          <div class="flex text-gray-700 px-6 py-5 text-xl font-bold">Upcomming</div>
+          <div
+            class="flex items-center justify-between px-6 py-4 text-sm border-b border-gray-300 cursor-pointer">
+            <div class="text-gray-700 text-lg font-semibold">
+              Hehe
+            </div>
+          </div>
+        </div>
+        <div class="flex flex-col">
+          <div class="flex text-gray-700 px-6 py-5 text-xl font-bold">Finished</div>
+          <div
+            class="flex items-center justify-between px-6 py-4 text-sm border-b border-gray-300 cursor-pointer">
+            <div class="text-gray-700 text-lg font-semibold">
+              Hehe
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Options, Vue } from 'vue-class-component'
 
 @Options({
-  components: {
-    HelloWorld,
-  },
+  components: {},
 })
 export default class Home extends Vue {}
 </script>
